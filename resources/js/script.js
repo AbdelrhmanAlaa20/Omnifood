bd53c68bd96fda4fa70edfaa788d265f67150a1e
AbdelrhmanAlaa <boody1749@gmail.com>
2021-05-07 12:59:38 +0200

    dadyh

--- resources/js/script.js
+++ resources/js/script.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
     $('.features').waypoint(function(direction) {
         $('.features').addClass('animate__fadeInUp , animate__animated');
     }, {
-        offset: '50%'
+        offset: '65%'
     });
     $('.app').waypoint(function(direction) {
         $('.app').addClass('animate__fadeInUp , animate__animated');
@@ -53,20 +53,5 @@ $(document).ready(function() {
         offset: '50%'
     });
 
-    
-    /* Mobile navigation */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-        
-        nav.slideToggle(200);
-        
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }        
-    });
+ 
 });
